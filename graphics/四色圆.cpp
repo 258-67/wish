1d4dc66dffff8642729b2532b02f5e993aa05e58
yanjingyu <2767563749@qq.com>
2022-01-09 15:39:46 +0800

    “旋转蛇“基本单元

---
+++ graphics/四色圆.cpp
@@ -0,0 +1,24 @@
+//graphics.h文件内solidpie函数
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+#define PI 3.1415926
+int main()
+{
+	initgraph(600,600);//初始化600*600像素的幕布
+	setbkcolor(RGB(128,128,128));//设置背景颜色
+	cleardevice();//以背景颜色清空幕布
+	int color_x=300,color_y=300,radius=200;
+	int left=color_x-radius,top=color_y-radius,right=color_x+radius,bottom=color_y+radius;
+	setfillcolor(RGB(0,255,0));
+	solidpie(left,top,right,bottom,0,PI/2);
+	setfillcolor(RGB(255,255,255,255));
+	solidpie(left,top,right,bottom,PI/2,PI);
+	setfillcolor(RGB(255,0,0));
+	solidpie(left,top,right,bottom,PI,PI*3/2);
+	setfillcolor(RGB(0,0,0));
+	solidpie(left,top,right,bottom,PI*3/2,PI*2);
+	_getch();
+	closegraph;
+	return(0);
+}
---
+++ graphics/渐变同心圆.cpp
@@ -0,0 +1,19 @@
+//½¥±äÍ¬ĞÄÔ²
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+int main()
+{
+	initgraph(600,600);
+	setbkcolor(RGB(255,255,255));
+	cleardevice();
+	int radius;
+	for(radius=255;radius>10;radius-=10)
+	{
+	setlinecolor(RGB(255-radius,radius,radius));
+	circle(300,300,255-radius);
+	}
+	_getch();
+	closegraph;
+	return(0);
+}
